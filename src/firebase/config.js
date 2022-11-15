4fb43da6fd573fdd6377c4d79724e63461f33e1a
DaniMatuko <matuko305@gmail.com>
2022-11-15 09:50:06 +0200

    firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCAVYA-G_NXJp0Z1O3VLkThKchrCmWR_kY",
+  authDomain: "firetask-danimatuko.firebaseapp.com",
+  projectId: "firetask-danimatuko",
+  storageBucket: "firetask-danimatuko.appspot.com",
+  messagingSenderId: "660507493002",
+  appId: "1:660507493002:web:08910bfed48f7fdbe12373",
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init services
+const db = firebase.firestore();
+const auth = firebase.auth();
+
+// timestamp
+const timestamp = firebase.firestore.Timestamp();
+
+export { db, auth, timestamp };
