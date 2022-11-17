6f27fd1d47035fa26997d4f6e5239e09792a75bf
DaniMatuko <matuko305@gmail.com>
2022-11-17 13:21:08 +0200

    protect routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,41 @@
-import { useState } from "react";
-import { Route, Routes } from "react-router-dom";
+import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Create from "./pages/Create";
 import Project from "./pages/Project";
 import Dashboard from "./pages/Dashboard";
+import { useAuthContext } from "./hooks/useAuthContext";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const { authIsReady, user } = useAuthContext();
 
   return (
     <div className='App'>
-      <Routes>
-        <Route
-          path='/'
-          element={<Dashboard />}
-        />{" "}
-        <Route
-          path='/login'
-          element={<Login />}
-        />
-        <Route
-          path='/signup'
-          element={<Signup />}
-        />
-        <Route
-          path='/projects/:id'
-          element={<Project />}
-        />
-        <Route
-          path='/create'
-          element={<Create />}
-        />
-      </Routes>
+      {authIsReady && (
+        <Routes>
+          <Route
+            path='/'
+            element={user ? <Dashboard /> : <Navigate to='/login' />}
+          />
+          <Route
+            path='/login'
+            element={!user ? <Login /> : <Navigate to='/' />}
+          />
+          <Route
+            path='/signup'
+            element={!user ? <Signup /> : <Navigate to='/' />}
+          />
+          <Route
+            path='/projects/:id'
+            element={user ? <Project /> : <Navigate to='/login' />}
+          />
+          <Route
+            path='/create'
+            element={user ? <Create /> : <Navigate to='/login' />}
+          />
+        </Routes>
+      )}
     </div>
   );
 }
